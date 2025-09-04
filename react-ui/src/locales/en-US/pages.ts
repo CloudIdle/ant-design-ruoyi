7b1925975bdde959b296825604c6c9f40d022089
zzz <1483713539@qq.com>
2025-09-04 10:31:11 +0800

    初始化

--- react-ui/src/locales/en-US/pages.ts
+++ react-ui/src/locales/en-US/pages.ts
@@ -1,6 +1,6 @@
 export default {
   'pages.layouts.userLayout.title':
-    'Ant Design is the most influential web design specification in Xihu district',
+    'Central control management platform',
   'pages.login.accountLogin.tab': 'Account Login',
   'pages.login.accountLogin.errorMessage': 'Incorrect username/password(admin/admin123)',
   'pages.login.failure': 'Login failed, please try again!',
